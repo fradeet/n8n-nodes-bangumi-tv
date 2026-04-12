6e006d7bfa0b8a69c02c9bc41e6c2140a6c690d1
fradeet <41571697+fradeet@users.noreply.github.com>
2026-04-12 11:08:30 +0800

    refactor: replace Collection with Me resource in descriptions

--- nodes/BangumitvApi/actions/descriptions.ts
+++ nodes/BangumitvApi/actions/descriptions.ts
@@ -4,7 +4,7 @@ import { description as episodeDescription } from './episode/Episode.resource';
 import { description as characterDescription } from './character/Character.resource';
 import { description as personDescription } from './person/Person.resource';
 import { description as userDescription } from './user/User.resource';
-import { description as collectionDescription } from './collection/Collection.resource';
+import { description as meDescription } from './me/Me.resource';
 import { description as revisionDescription } from './revision/Revision.resource';
 import { description as indexDescription } from './index/Index.resource';
 
@@ -15,13 +15,13 @@ export const properties: INodeProperties[] = [
 		type: 'options',
 		noDataExpression: true,
 		options: [
-			{ name: 'Character', value: 'character', description: 'Fictional person in story' },
-			{ name: 'Collection', value: 'collection', description: 'Items saved by the user' },
-			{ name: 'Episode', value: 'episode', description: 'A part of a series' },
-			{ name: 'Index', value: 'index', description: 'Personalized collection saved by the user' },
-			{ name: 'Person', value: 'person', description: 'Real-life individual' },
-			{ name: 'Revision', value: 'revision', description: 'Wiki edit history' },
-			{ name: 'Subject', value: 'subject', description: 'An individual entry or item' },
+			{ name: 'Character', value: 'character' },
+			{ name: 'Episode', value: 'episode' },
+			{ name: 'Index', value: 'index' },
+			{ name: 'Me', value: 'me' },
+			{ name: 'Person', value: 'person' },
+			{ name: 'Revision', value: 'revision' },
+			{ name: 'Subject', value: 'subject' },
 			{ name: 'User', value: 'user' },
 		],
 		default: 'subject',
@@ -31,7 +31,7 @@ export const properties: INodeProperties[] = [
 	...characterDescription,
 	...personDescription,
 	...userDescription,
-	...collectionDescription,
+	...meDescription,
 	...revisionDescription,
 	...indexDescription,
 ];
