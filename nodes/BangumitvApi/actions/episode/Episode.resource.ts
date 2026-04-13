9b8c0a98ad3663e7806a7d3a2c74a67a4a06af7b
fradeet <41571697+fradeet@users.noreply.github.com>
2026-04-13 10:57:42 +0800

    feat: add collection status operations to Episode resource

--- nodes/BangumitvApi/actions/episode/Episode.resource.ts
+++ nodes/BangumitvApi/actions/episode/Episode.resource.ts
@@ -1,6 +1,10 @@
 import type { INodeProperties } from 'n8n-workflow';
 import { properties as getManyProps } from './getMany.operation';
 import { properties as getProps } from './get.operation';
+import { properties as getCollectionProps } from './getCollection.operation';
+import { properties as getCollectionsProps } from './getCollections.operation';
+import { properties as updateCollectionProps } from './updateCollection.operation';
+import { properties as updateCollectionsProps } from './updateCollections.operation';
 
 export const description: INodeProperties[] = [
 	{
@@ -16,10 +20,34 @@ export const description: INodeProperties[] = [
 				description: 'Retrieve an episode by ID',
 			},
 			{
+				name: 'Get Collection Status',
+				value: 'getCollection',
+				action: 'Get episode collection status',
+				description: 'Retrieve your watch status for an episode',
+			},
+			{
 				name: 'Get Many',
 				value: 'getMany',
 				action: 'Get many episodes',
 				description: 'Retrieve episodes for a subject',
+			},
+			{
+				name: 'Get Many Collection Statuses',
+				value: 'getCollections',
+				action: 'Get many episode collection statuses',
+				description: 'Retrieve episode watch statuses for a subject',
+			},
+			{
+				name: 'Update Collection Status',
+				value: 'updateCollection',
+				action: 'Update episode collection status',
+				description: 'Update your watch status for an episode',
+			},
+			{
+				name: 'Update Collection Statuses',
+				value: 'updateCollections',
+				action: 'Update episode collection statuses',
+				description: 'Batch update watch statuses for episodes',
 			},
 		],
 		default: 'getMany',
@@ -31,4 +59,8 @@ export const description: INodeProperties[] = [
 	},
 	...getManyProps,
 	...getProps,
+	...getCollectionProps,
+	...getCollectionsProps,
+	...updateCollectionProps,
+	...updateCollectionsProps,
 ];
---
+++ nodes/BangumitvApi/actions/episode/getCollections.operation.ts
@@ -0,0 +1,43 @@
+import type { INodeProperties } from 'n8n-workflow';
+import {
+	episodeCollectionLimitProperty,
+	offsetProperty,
+	episodeTypeOptions,
+} from '../common.descriptions';
+
+export const properties: INodeProperties[] = [
+	{
+		displayName: 'Subject ID',
+		name: 'subjectId',
+		type: 'string',
+		required: true,
+		default: '',
+		routing: {
+			request: {
+				method: 'GET',
+				url: '=/v0/users/-/collections/{{$value}}/episodes',
+			},
+		},
+		displayOptions: { show: { resource: ['episode'], operation: ['getCollections'] } },
+	},
+	{
+		displayName: 'Episode Type',
+		name: 'episodeType',
+		type: 'options',
+		options: episodeTypeOptions,
+		default: 0,
+		description: 'Filter by episode type',
+		displayOptions: { show: { resource: ['episode'], operation: ['getCollections'] } },
+		routing: { request: { qs: { episode_type: '={{$value}}' } } },
+	},
+	{
+		...episodeCollectionLimitProperty,
+		displayOptions: { show: { resource: ['episode'], operation: ['getCollections'] } },
+		routing: { request: { qs: { limit: '={{$value}}' } } },
+	},
+	{
+		...offsetProperty,
+		displayOptions: { show: { resource: ['episode'], operation: ['getCollections'] } },
+		routing: { request: { qs: { offset: '={{$value}}' } } },
+	},
+];
